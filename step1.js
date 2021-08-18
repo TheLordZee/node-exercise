3d340e103de0268b752f5696e09ca4c2ff0edd28
LordZee <l0rclz33@gmail.com>
2021-08-18 15:31:37 -0400

    step 1 done

---
+++ step1.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+let path = process.argv[2]
+
+function cat(path){
+    fs.readFile(path, 'utf8', function(err, data){
+        if(err){
+            console.log(err);
+            process.exit(1)
+        }
+        console.log(data)
+    })
+}
+
+cat(path)
+
+module.exports = {
+    cat
+};
